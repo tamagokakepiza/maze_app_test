4f39ee86f0ea4bc6a2dded35af091267c6245382
tamagokakepiza <nyororo89@gmail.com>
2022-03-18 23:46:53 +0900

    迷路の一ブロックの定義

--- maze_test.c
+++ maze_test.c
@@ -1,5 +1,11 @@
-
+#include <stdio.h>
 
 //迷路のーブロック
 enum MazeKind {PATH, WALL, START, GOAL};    //ブロックの種類(道、壁、スタート、ゴール)
-enum MazeFlag {}
+enum MazeFlag {FALSE, TRUE};                //ブロックが判明しているかどうか
+
+typedef struct
+{
+  enum MazeKind kind;                       //種類(道、壁、スタート、ゴール)
+  enum MazeFlag flag;                       //ブロックが判明しているかどうか
+} MazeBlock;
