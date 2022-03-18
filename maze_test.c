82b8ffb2a5e45a33bb59e51399fd5969725713f9
tamagokakepiza <nyororo89@gmail.com>
2022-03-19 02:31:24 +0900

    迷路の配列作成
    This reverts commit 29b151c4d3ff8338f5544bfec197254ef54bb53e.

--- maze_test.c
+++ maze_test.c
@@ -9,3 +9,16 @@ typedef struct
   enum MazeKind kind;                       //種類(道、壁、スタート、ゴール)
   enum MazeFlag flag;                       //ブロックが判明しているかどうか
 } MazeBlock;
+
+//迷路
+#define MAZE_ROW    5                       //迷路の行数
+#define MAZE_COLUMN 5                       //迷路の列数
+
+  MazeBlock maze[MAZE_ROW][MAZE_COLUMN] = 
+    {
+      { {START, TRUE } , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE} },
+      { {WALL , FALSE} , {WALL , FALSE}, {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE} },
+      { {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE} },
+      { {PATH , FALSE} , {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {WALL , FALSE} },
+      { {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {GOAL , TRUE } },
+    };
