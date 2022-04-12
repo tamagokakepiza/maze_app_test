816cfe01faada81536d797e290946e5506487a04
tamagokakepiza <nyororo89@gmail.com>
2022-04-12 14:45:19 +0900

    プレイヤーの表示

--- maze_test.c
+++ maze_test.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-#define MAZE_ROW    5                       //迷路の行数
-#define MAZE_COLUMN 5                       //迷路の列数
+#define MAZE_ROW    5                         //迷路の行数
+#define MAZE_COLUMN 5                         //迷路の列数
 
 //迷路のーブロック
-enum MazeKind {PATH, WALL, START, GOAL};    //ブロックの種類(道、壁、スタート、ゴール)
-enum MazeFlag {FALSE, TRUE};                //ブロックの判定
+enum MazeKind {PATH, WALL, START, GOAL};      //ブロックの種類(道、壁、スタート、ゴール)
+enum MazeFlag {FALSE, TRUE};                  //ブロックの判定
 typedef struct {
-  enum MazeKind kind;                       //種類(道、壁、スタート、ゴール)
-  enum MazeFlag flag;                       //ブロックの判定
+  enum MazeKind kind;                         //種類(道、壁、スタート、ゴール)
+  enum MazeFlag flag;                         //ブロックの判定
 } MazeBlock;
 
 //プレイヤー
 typedef struct {
-  int row;                                  //プレイヤー位置(行)
-  int column;                               //プレイヤー位置(列)
+  int row;                                    //プレイヤー位置(行)
+  int column;                                 //プレイヤー位置(列)
 } MazePlayer;
 
 //プレイヤーの初期化
 int MazePlayerInit(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][MAZE_COLUMN]) {
   int i, j;
 
-  for(i = 0; i < MAZE_ROW; i++) {         //行
-    for(j = 0; j < MAZE_COLUMN; j++) {    //列
-      if(maze[i][j].kind == START) {                                               //スタート地点の場合
+  for(i = 0; i < MAZE_ROW; i++) {             //行
+    for(j = 0; j < MAZE_COLUMN; j++) {        //列
+      if(maze[i][j].kind == START) {          //スタート地点の場合
         *playerRow = i;
         *playerColumn = j;
         return 0;
@@ -31,27 +31,29 @@ int MazePlayerInit(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][M
     }
   }
 
-  printf("スタートがありません\n");                                                   //スタート地点がない場合
+  printf("スタートがありません\n");               //スタート地点がない場合
   return -1;
 }
 
 //迷路表示
-void MazeDraw(MazeBlock maze[MAZE_ROW][MAZE_COLUMN]) {
+void MazeDraw(int playerRow, int playerColumn, MazeBlock maze[MAZE_ROW][MAZE_COLUMN]) {
   int i, j;
-  for(i = 0; i < MAZE_ROW; i++) {           //行
-    for(j = 0; j < MAZE_COLUMN; j++) {      //列
-      if(maze[i][j].flag == FALSE) {        //ブロックが判明していない場合
+  for(i = 0; i < MAZE_ROW; i++) {             //行
+    for(j = 0; j < MAZE_COLUMN; j++) {        //列
+      if(playerRow == i && playerColumn == j) {
+        printf("P");                          //プレイヤー位置
+      } else if(maze[i][j].flag == FALSE) {   //ブロックが判明していない場合
         printf("?");
       } else {
         switch (maze[i][j].kind) {
         case WALL:
-          printf("□");                      //壁
+          printf("□");                        //壁
           break;
         case GOAL:
-          printf("G");                      //ゴール
+          printf("G");                        //ゴール
           break;
         default:
-          printf(" ");                      //その他(スタート、道)
+          printf(" ");                        //その他(スタート、道)
           break;
         }
       }
@@ -63,12 +65,12 @@ void MazeDraw(MazeBlock maze[MAZE_ROW][MAZE_COLUMN]) {
 int main(void) {
   //迷路
   MazeBlock maze[MAZE_ROW][MAZE_COLUMN] = {
-      { {START, TRUE } , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE} },
-      { {WALL , FALSE} , {WALL , FALSE}, {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE} },
-      { {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE} },
-      { {PATH , FALSE} , {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {WALL , FALSE} },
-      { {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {GOAL , TRUE } },
-    };
+    { {START, TRUE } , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE} },
+    { {WALL , FALSE} , {WALL , FALSE}, {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE} },
+    { {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE} },
+    { {PATH , FALSE} , {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {WALL , FALSE} },
+    { {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {GOAL , TRUE } },
+  };
 
   //迷路表示
   MazeDraw(maze);
